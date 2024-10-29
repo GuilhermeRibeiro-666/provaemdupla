6c7d08eb80baad22f1c4c7fc2782f4c595e57d31
GuilhermeRibeiro-666 <guilhermeribeiroifba@gmail.com>
2024-10-29 10:50:56 -0300

    Botao e Estilo CSS

--- index.php
+++ index.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="css.css">
+
 </head>
 <body>
+
     <h1> Guilherme Ribeiro Alves de Melo e Raphael Gonçalves de Mundo </h1>
 
     <img src="Imagens/fotopizza.jpg">
+
+    <a href="cadastro.php"> Botao</a>
 </body>
 </html>
